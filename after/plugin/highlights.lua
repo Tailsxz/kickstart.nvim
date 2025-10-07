b0180c36cb8105b46ec7e5bacd4e1b47bd02decb
Tailsxz <ptai7722@gmail.com>
2025-10-07 13:16:24 -0500

    add: custom lisp highlight colors

--- after/plugin/highlights.lua
+++ after/plugin/highlights.lua
@@ -10,3 +10,8 @@ vim.cmd [[
   highlight MyCursorLine guifg=#00DDEE guibg=#110033 |
   highlight CmpItemMenu guifg=#225599 ctermfg=Yellow
 ]]
+
+vim.api.nvim_set_hl(0, '@custom.commonlisp', { fg = '#61E294' })
+vim.api.nvim_set_hl(0, '@function.commonlisp', { fg = '#6FBFF9' })
+vim.api.nvim_set_hl(0, '@function.builtin.commonlisp', { fg = '#FCD29F' })
+vim.api.nvim_set_hl(0, '@function.userdefined', { fg = '#6FBFF9' })
